16acb4d9405449c8cdfe1b22c9ebf884ba69e782
Carson <carsoncolgateccu@gmail.com>
2021-09-20 13:39:30 -0600

    added JS with my questions and answers

---
+++ questionscript.js
@@ -0,0 +1,70 @@
+var questions=[
+    {
+        numb: 1,
+        question: "In Frozen, What is the name of the Reindeer?",
+        answer: "Sven",
+        options: [
+            "Sven",
+            "Olaf",
+            "Steven",
+            "Rudolph",
+        ],
+    },
+    
+    {
+        numb: 2,
+        question: "In Jerry Maguire what is the iconic saying",
+        answer: "Show me the Money!",
+        options: [
+            "Stand with me!",
+            "No days off!",
+            "Show me the Money!",
+            "Give me everything you got!",
+        ],
+    },
+    {
+        numb: 3,
+        question: "Who plays the Main character in the Movie Drive",
+        answer: "Ryan Gosling",
+        options: [
+            "Ryan Reynolds",
+            "Brad Pitt",
+            "Bradley Cooper",
+            "Ryan Gosling",
+        ],
+    },
+    {
+        numb: 4,
+        question: "In the Movie Interstellar what is the name of Cooper's(Matthew Mconaughey) daughter?",
+        answer: "Murph",
+        options: [
+            "Molly",
+            "Meg",
+            "Murph",
+            "Missy",
+        ],
+    },
+    {
+        numb: 5,
+        question: "In Wolf of Wall Street what real life millionaire is it about?",
+        answer: "Jordan Belfort",
+        options: [
+            "Jeff Bezos",
+            "Elon Musk",
+            "Bill Gates",
+            "Jordan Belfort",
+        ],
+    },
+    {
+        numb: 6,
+        question: "Witch actor is not in the movie Inception",
+        answer: "Jason Clarke",
+        options: [
+            "Leonardo DiCaprio",
+            "Michael Caine",
+            "Cillian Murphy",
+            "Jason Clarke",
+        ],
+    },
+    
+]

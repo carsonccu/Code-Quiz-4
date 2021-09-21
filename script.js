9014d670f4f904ce4d9b84b20cd8fe3f223b6ab2
Carson <carsoncolgateccu@gmail.com>
2021-09-21 12:42:23 -0600

    added buttons in my html for all my answers to my questions

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const submitbutton= document.getElementById('submit');
 
 function buildquiz(){}
 
+
 function showresults(){}
 
 buildquiz();
@@ -13,65 +14,65 @@ submitbutton.addEventListener('click, showresults');
 const myquestions = [
         {
         question: "In Frozen, What is the name of the Reindeer?",
-        answer: "Sven",
         options: {
            a: "Sven",
            b: "Olaf",
            c: "Steven",
            d: "Rudolph",
         },
+        correctanswer:"a"
     },
     
     {
         question: "In Jerry Maguire what is the iconic saying",
-        answer: "Show me the Money!",
         options: {
            a: "Stand with me!",
            b: "No days off!",
            c: "Show me the Money!",
            d: "Give me everything you got!",
         },
+        correctanswer:"c" 
     },
     {
         question: "Who plays the Main character in the Movie Drive",
-        answer: "Ryan Gosling",
         options: {
            a: "Ryan Reynolds",
            b: "Brad Pitt",
            c: "Bradley Cooper",
            d: "Ryan Gosling",
         },
+        correctanswer:"d"
     },
     {
         numb: 4,
         question: "In the Movie Interstellar what is the name of Cooper's(Matthew Mconaughey) daughter?",
-        answer: "Murph",
         options: {
            a: "Molly",
            b: "Meg",
            c: "Murph",
            d: "Missy",
         },
+        correctanswer:"c"
     },
     {
         question: "In Wolf of Wall Street what real life millionaire is it about?",
-        answer: "Jordan Belfort",
         options: {
            a: "Jeff Bezos",
            b: "Elon Musk",
            c: "Bill Gates",
            d: "Jordan Belfort",
         },
+        correctanswer:"d"
     },
     {
         question: "Witch actor is not in the movie Inception",
-        answer: "Jason Clarke",
         options: {
            a: "Leonardo DiCaprio",
            b: "Michael Caine",
            c: "Cillian Murphy",
            d: "Jason Clarke",
         },
+        correctanswer:"d"
     },
     
 ]

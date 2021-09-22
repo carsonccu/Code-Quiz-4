69c6bf932282d4672af58287a472d64d4cfa3d0e
Carson <carsoncolgateccu@gmail.com>
2021-09-22 17:06:23 -0600

    started a function with help of tutor to show questions and answers

--- script.js
+++ script.js
@@ -1,48 +1,91 @@
 const startButton = document.getElementById('start-button');
 const questionContainer = document.getElementById('question-container');
 const questionElement = document.getElementById("question")
-const answerElement = document.getElementById("answer-button")
 var correctAnswer = document.getElementById("correct-answer")
 var wrongAnswer = document.getElementById("wrong-answer")
 var gameOver = document.getElementById("game-over")
-var localScore= document.getElementById("local-score")
+var localScore = document.getElementById("local-score")
 var timeLeft = document.getElementById("timer-text")
+var questionText = document.getElementById("questions");
+var currentindex = 0
+var timeLeft = 120
 
 const myquestions = [
-        {
+    {
         question: "In Frozen, What is the name of the Reindeer?",
-        options: [ "Sven", "Olaf", "Steven", "Rudolph"],
-           answer: "Sven"
-        },
+        options: ["Sven", "Olaf", "Steven", "Rudolph"],
+        answer: "Sven"
+    },
     {
         question: "In Jerry Maguire what is the iconic saying",
         options: ["Stand with me!", "No days off!", "Show me the Money!", "Give me evertything you got!"],
-           answer: "Show me the Money"
+        answer: "Show me the Money"
     },
     {
         question: "Who plays the Main character in the Movie Drive",
-        options: ["Ryan Reynolds","Brad pitt", "Bradley Cooper","Ryan Gosling"],
-           answer: "Ryan Gosling"
+        options: ["Ryan Reynolds", "Brad pitt", "Bradley Cooper", "Ryan Gosling"],
+        answer: "Ryan Gosling"
     },
     {
         question: "In the Movie Interstellar what is the name of Cooper's(Matthew Mconaughey) daughter?",
-        options: ["Molly","Meg","Murph","Missy",],
-            answer:"Murph"
+        options: ["Molly", "Meg", "Murph", "Missy",],
+        answer: "Murph"
     },
     {
         question: "Witch actor is not in the movie Inception",
-        options: ["Leonardo DiCaprio","Michael Caine","Cillian Murphy","Jason Clarke"],
-            answer:"Jason Clarke"
+        options: ["Leonardo DiCaprio", "Michael Caine", "Cillian Murphy", "Jason Clarke"],
+        answer: "Jason Clarke"
     },
-    
+
 ]
 
 startButton.addEventListener('click', startQuiz)
 
-function startQuiz(){
-    for (var i=0; i < myquestions.length; i++)
-    return startQuiz
+
+
+function startQuiz() {
+    startButton.classList.add('hide')
+    // for (var i=0; i < myquestions[currentindex].options.length; i++){
+    //     console.log()
+    // }
+    questionContainer.classList.remove('hide')
+    showQuestion()
 }
+function showQuestion() {
+    var question = myquestions[currentindex]
+    questionElement.innerText = question.question
+    // for (var i=0; i <qestion.options.length; i++){
+    //     console.log()
+    question.options.forEach(option => {
+        console.log(option, question.answer, option === question.answer)
+        const button = document.createElement("button")
+        button.innerText = option
+        button.classList.add("btn")
+        if (question.answer === option) {
+            button.dataset.correct = ("correct")
+        }
+        button.addEventListener("click", selectAnswer)
+        questionContainer.appendChild(button)
+    })
+}
+
+
+function timer() { }
+
+function selectAnswer(event) {
+    var correctAnswer = event.target.dataset.correct
+    if (correctAnswer) {
+        console.log("correctAnswer")
+    } else {
+        console.log("wrongAnswer")
+    }
+    currentindex++
+    showQuestion()
+}
+
+function checkAnswer() { }
+
+function finalscore() { }
 
 
 
@@ -68,7 +111,7 @@ function startQuiz(){
     // }
     // function setnextQuestion(){
     //     showQuestion(shuffledQuestions[currentQuestionIndex])
-    
+
     // function showQuestion(question){
     //     questionElement.innerText = question.question
     //     question.answers.forEach(answer => {
@@ -81,7 +124,7 @@ function startQuiz(){
     //     button.addEventListener("click", selectAnswer)
     //     answerElement.appendChild(button)
     //     })
-        
+
     // }
     // function selectAnswer(){
     // }

e833d7dce81d296597dec3ed0a5f298e316277f7
Carson <carsoncolgateccu@gmail.com>
2021-09-22 12:43:52 -0600

    put all my answers into 1 array

--- script.js
+++ script.js
@@ -12,98 +12,59 @@ let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
-function startGame(){
-    startButton.classList.add('hide')
-    shuffledQuestions = myquestions.sort(()=> Math.random()-.5)
-    currentQuestionIndex=0
-    questionContainer.classList.remove('hide')
-    setnextQuestion()
-}
-function setnextQuestion(){
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-
-function showQuestion(question){
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-    const button = document.createElement("button")
-    button.innerText= answer.text
-    button.classList.add("btn")
-    if (answer.correct) {
-        button.dataset.correct = answer.correct
-    }
-    button.addEventListener("click", selectAnswer)
-    answerElement.appendChild(button)
-    })
-    
-}
-function selectAnswer(e){
-}
 
 const myquestions = [
         {
         question: "In Frozen, What is the name of the Reindeer?",
-        answers: [
-           {text: "a: Sven"},
-           {text: "Olaf"},
-           {text: "Steven"},
-           {text: "Rudolph"},
-        ]
-        }
-    ]
-//         correctanswer:"a"
-//     },
+        options: [ "Sven", "Olaf", "Steven", "Rudolph"],
+           answer: "Sven"
+        },
+    {
+        question: "In Jerry Maguire what is the iconic saying",
+        options: ["Stand with me!", "No days off!", "Show me the Money!", "Give me evertything you got!"],
+           answer: "Show me the Money"
+    },
+    {
+        question: "Who plays the Main character in the Movie Drive",
+        options: ["Ryan Reynolds","Brad pitt", "Bradley Cooper","Ryan Gosling"],
+           answer: "Ryan Gosling"
+    },
+    {
+        question: "In the Movie Interstellar what is the name of Cooper's(Matthew Mconaughey) daughter?",
+        options: ["Molly","Meg","Murph","Missy",],
+            answer:"Murph"
+    },
+    {
+        question: "Witch actor is not in the movie Inception",
+        options: ["Leonardo DiCaprio","Michael Caine","Cillian Murphy","Jason Clarke"],
+            answer:"Jason Clarke"
+    },
     
-//     {
-//         question: "In Jerry Maguire what is the iconic saying",
-//         options: {
-//            a: "Stand with me!",
-//            b: "No days off!",
-//            c: "Show me the Money!",
-//            d: "Give me everything you got!",
-//         },
-//         correctanswer:"c" 
-//     },
-//     {
-//         question: "Who plays the Main character in the Movie Drive",
-//         options: {
-//            a: "Ryan Reynolds",
-//            b: "Brad Pitt",
-//            c: "Bradley Cooper",
-//            d: "Ryan Gosling",
-//         },
-//         correctanswer:"d"
-//     },
-//     {
-//         numb: 4,
-//         question: "In the Movie Interstellar what is the name of Cooper's(Matthew Mconaughey) daughter?",
-//         options: {
-//            a: "Molly",
-//            b: "Meg",
-//            c: "Murph",
-//            d: "Missy",
-//         },
-//         correctanswer:"c"
-//     },
-//     {
-//         question: "In Wolf of Wall Street what real life millionaire is it about?",
-//         options: {
-//            a: "Jeff Bezos",
-//            b: "Elon Musk",
-//            c: "Bill Gates",
-//            d: "Jordan Belfort",
-//         },
-//         correctanswer:"d"
-//     },
-//     {
-//         question: "Witch actor is not in the movie Inception",
-//         options: {
-//            a: "Leonardo DiCaprio",
-//            b: "Michael Caine",
-//            c: "Cillian Murphy",
-//            d: "Jason Clarke",
-//         },
-//         correctanswer:"d"
-//     },
+]
+
+// function startGame(){
+    //     startButton.classList.add('hide')
+    //     shuffledQuestions = myquestions.sort(()=> Math.random()-.5)
+    //     currentQuestionIndex=0
+    //     questionContainer.classList.remove('hide')
+    //     setnextQuestion()
+    // }
+    // function setnextQuestion(){
+    //     showQuestion(shuffledQuestions[currentQuestionIndex])
     
-// ]
-}
+    // function showQuestion(question){
+    //     questionElement.innerText = question.question
+    //     question.answers.forEach(answer => {
+    //     const button = document.createElement("button")
+    //     button.innerText= answer.text
+    //     button.classList.add("btn")
+    //     if (answer.correct) {
+    //         button.dataset.correct = answer.correct
+    //     }
+    //     button.addEventListener("click", selectAnswer)
+    //     answerElement.appendChild(button)
+    //     })
+        
+    // }
+    // function selectAnswer(){
+    // }
